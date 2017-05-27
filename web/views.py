3a9fa98aadd2f787d91dd57479da0a19975526d1
Andres Root <info@andresroot.co>
2017-05-27 15:00:45 -0500

    working on api

--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
 # Create your views here.
 
 
@@ -8,4 +9,6 @@ def index(request):
 
 
 def payment(request):
-    return HttpResponse("Paying...")
+    id = request.GET.get('id', '')
+    response = {'id': id}
+    return JsonResponse(response)
