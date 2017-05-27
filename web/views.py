6731e952195bcd1898c9460e3ca2e0ec4e6513d8
Andres Root <info@andresroot.co>
2017-05-27 18:23:24 -0500

    Working on transactions

--- web/views.py
+++ web/views.py
@@ -1,7 +1,6 @@
 # from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
-# Create your views here.
+from .models import Bar, Client
 
 
 def index(request):
@@ -9,13 +8,32 @@ def index(request):
 
 
 def payment(request):
+    total = 0
     phone = request.GET.get('phone', '')
-    bar = request.GET.get('bar', '')
+    barname = request.GET.get('bar', '')
     table = request.GET.get('table', '')
     beer_amount = request.GET.get('beer', '')
     split_fare = request.GET.get('split', '')
 
-    if split_fare != '':
-        split_mates = split_fare.split(',')
-    response = {'status': 'ok'}
-    return JsonResponse(response)
+    try:
+        bar = Bar.objects.get(username=barname)
+        client = Bar.objects.get(phone=phone)
+        if split_fare != '':
+            split_mates = split_fare.split(',')
+            total = (bar.beer_cost * beer_amount) / len(split_mates)
+            client.balance = client.balance - total
+            client.save()
+        else:
+            total = bar.beer_cost * beer_amount
+            client.balance = client.balance - total
+            client.save()
+
+        response = {'status': 'ok'}
+        return JsonResponse(response)
+    except:
+        error_message = 'An error has ocurred. Please try again.'
+        response = {
+            'status': 'error',
+            'message': error_message
+        }
+        return JsonResponse(response)
