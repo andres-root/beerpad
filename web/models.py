520e1eb02035a0d0a1dcf73f1db15ccab5248352
Andres Root <info@andresroot.co>
2017-05-27 16:14:48 -0500

    working on Models

--- web/models.py
+++ web/models.py
@@ -7,8 +7,8 @@ class Client(models.Model):
     name = models.CharField(max_length=400)
     phone = models.CharField(max_length=200)
     balance = models.DecimalField(max_digits=6, decimal_places=2)
-    date_created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    date_updated = models.DateTimeField(auto_now_add=True, auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -18,8 +18,8 @@ class Bar(models.Model):
     name = models.CharField(max_length=400)
     phone = models.CharField(max_length=200)
     balance = models.DecimalField(max_digits=6, decimal_places=2)
-    date_created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    date_updated = models.DateTimeField(auto_now_add=True, auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -27,14 +27,14 @@ class Bar(models.Model):
 
 class Transactions(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    bar = models.ForeignKey(Client, on_delete=models.CASCADE)
+    bar = models.ForeignKey(Bar, on_delete=models.CASCADE)
     table = models.CharField(max_length=200)
     discounts = models.DecimalField(max_digits=6, decimal_places=2)
     beer_amount = models.FloatField()
     subtotal = models.DecimalField(max_digits=6, decimal_places=2)
     total = models.DecimalField(max_digits=6, decimal_places=2)
-    date_created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    date_updated = models.DateTimeField(auto_now_add=True, auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
